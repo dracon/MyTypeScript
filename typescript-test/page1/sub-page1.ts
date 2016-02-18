ff77ada6d8036df504110a5b091a217fa1b1503d
dracon <dracon64@gmail.com>
2016-02-18 18:54:52 +0100

    add semantic to gulp / change folder name src

--- typescript-test/page1/sub-page1.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from 'angular2/core';
-
-@Component ({
-  selector: 'sub-page1',
-  templateUrl: "./page1/sub-page1.html"
-})
-export class SubPage1{
-  title:string = "SubPage1";
-}
